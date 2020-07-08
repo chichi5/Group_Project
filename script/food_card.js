ce3aca2fe8b49cdb67ba61f5baa72016df2d3a48
trrt7676 <e823e823@yahoo.com.tw>
2020-07-08 16:59:49 +0800

    js start

---
+++ script/food_card.js
@@ -0,0 +1,9 @@
+var fctbtns = document.querySelectorAll('.fct_btn')
+var tcards = document.querySelectorAll('.card_style_t')
+
+for (let i = 0; i < fctbtns.length; i++) {
+    const element = fctbtns[i];
+    const element = tcards[i];
+    console.log();
+       
+}
