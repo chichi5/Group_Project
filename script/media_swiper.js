08a1e99b512ae31abc7ad86d7a485e17e6caad20
c87 <ul4.jo6@aol.com>
2020-07-10 15:06:49 +0800

    update

--- script/media_swiper.js
+++ script/media_swiper.js
@@ -1,8 +1,8 @@
 var mySwiper = new Swiper('.swiper-container', {
-    initialSlide :0,
-    observer:true,//修改swiper自己或子元素時，自動初始化swiper
-    observeParents:true,//修改swiper的父元素時，自動初始化swiper
-    direction: 'horizontal', // 垂直切換選項
+    initialSlide: 0,
+    observer: true, // 修改swiper自己或子元素時，自動初始化swiper
+    observeParents: true, // 修改swiper的父元素時，自動初始化swiper
+    direction: 'horizontal', // 垂直切換選項            
     speed: 1000, // 切換速度
     loop: true, // 循環模式選項
 
@@ -10,12 +10,12 @@ var mySwiper = new Swiper('.swiper-container', {
     effect : 'coverflow', // 可設定為"fade"（淡入）"cube"（方塊）"coverflow"（3d流）"flip"（3d翻轉）
     slidesPerView: 2, // 頁面數量
     centeredSlides: true,
-    
+
     coverflow: {
-        rotate: 30,
-        stretch: 10,
-        depth: 60,
-        modifier: 2,
-        slideShadows : true
-    },            
-})
+        rotate: 0, // 滑動時旋轉角度
+        stretch: 100, // 聚合寬度
+        depth: 100, // 景深
+        modifier: 1, // 覆蓋疊加層數
+        slideShadows : false // 是否陰影
+    }
+});
