2198bd21d9230375a7644f6d79902be743ecce53
chichi5 <cchichic757@gmail.com>
2020-07-15 22:42:49 +0800

    更新

--- script/media_swiper.js
+++ script/media_swiper.js
@@ -14,6 +14,28 @@ var mySwiper = new Swiper('.media-swiper', {
 
     coverflowEffect: {
         rotate: 0, // 滑動時旋轉角度
+       
+
+        modifier: 1, // 覆蓋疊加層數
+        slideShadows : false // 是否陰影
+    }
+});
+var mySwiper = new Swiper('.media-swiper-3', {
+    initialSlide: 0,
+    observer: true, // 修改swiper自己或子元素時，自動初始化swiper
+    observeParents: true, // 修改swiper的父元素時，自動初始化swiper
+    direction: 'horizontal', // 垂直切換選項            
+    speed: 1000, // 切換速度
+    loop: true, // 循環模式選項
+    
+
+    // 滾動效果
+    effect: 'coverflow', // 可設定為"fade"（淡入）"cube"（方塊）"coverflow"（3d流）"flip"（3d翻轉）
+    slidesPerView: 1, // 頁面數量
+    centeredSlides: true,
+
+    coverflowEffect: {
+        rotate: 0, // 滑動時旋轉角度
         stretch: 100, // 聚合寬度
         depth: 100, // 景深
 
